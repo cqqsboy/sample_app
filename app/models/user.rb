47a6d45f6270050b83151b035657957baa0ea84c
wei <cqqsboy@gmail.com>
2013-07-10 22:06:51 +0800

    now finish 6.3.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,11 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
-  validates :name, presence: true, length: { maximum: 50 }
+  attr_accessible :email, :name,:password, :password_confirmation
+         has_secure_password
+
+
+	validates :name, presence: true, length: { maximum: 50 }
        
  	before_save { |user| user.email = email.downcase }
 
@@ -19,6 +22,9 @@ class User < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
 	uniqueness: { case_sensitive: false }
+
+	validates :password, presence: true, length: { minimum: 6 }
+	validates :password_confirmation, presence: true
 
 
 end
